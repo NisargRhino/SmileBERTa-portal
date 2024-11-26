8799bb5719ddeff63e6810b9a98184f3061e3fd0
141799170 <nisargs2018@gmail.com>
2024-11-26 11:28:22 -0800

    fixed drug_class

--- predict_drug_classification.py
+++ predict_drug_classification.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from transformers import RobertaTokenizer, RobertaForMaskedLM
 import torch
 import pandas as pd
+from Levenshtein import distance as levenshtein_distance
 
 # Load model and tokenizer
 #model = RobertaForMaskedLM.from_pretrained('NisargRhino/drug-classification')
@@ -10,7 +11,7 @@ import pandas as pd
 #model.eval()
 
 # Load unique tags for similarity comparison if needed
-unique_tags_df = pd.read_csv('./drug_classification_data_df.csv')
+unique_tags_df = pd.read_csv('./drugclassoptions_final.csv')
 unique_tags_list = unique_tags_df['tags'].tolist()
 
 #app = Flask(__name__)
@@ -29,12 +30,24 @@ def classify_smiles():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 """
+drug_class_options = pd.read_csv('./drugclassoptions.csv')['name'].tolist()
+predicted_smiles_intial = ""
 def predict_fragment_smiles(smiles, model, tokenizer, max_length=128):
     inputs = tokenizer(smiles, max_length=max_length, padding='max_length', truncation=True, return_tensors="pt")
     with torch.no_grad():
         outputs = model(input_ids=inputs['input_ids'], attention_mask=inputs['attention_mask'])
     predicted_ids = torch.argmax(outputs.logits, dim=-1)
-    predicted_smiles = tokenizer.decode(predicted_ids[0], skip_special_tokens=True)
+    predicted_smiles_initial = tokenizer.decode(predicted_ids[0], skip_special_tokens=True)
+    return predicted_smiles_initial
+
+def find_most_similar_option(predicted_smiles_initial, options):
+    min_distance = float('inf')
+    predicted_smiles = None
+    for option in options:
+        dist = levenshtein_distance(predicted_smiles_initial, option)
+        if dist < min_distance:
+            min_distance = dist
+            predicted_smiles = option
     return predicted_smiles
 
 # if __name__ == '__main__':
